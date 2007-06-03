2fe5e229b1afd5d0116b4fc8cd310b0cf5441dd5
Erez_Zadok <ezk@cs.sunysb.edu>
2007-06-03 02:27:37 -0400

    cleanup: rewrite new_dentry_private data more simply
    Also remove unnecessary variables and statements.
    
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- fs/unionfs/lookup.c
+++ fs/unionfs/lookup.c
@@ -483,23 +483,18 @@ int new_dentry_private_data(struct dentry *dentry)
 	int size;
 	struct unionfs_dentry_info *info = UNIONFS_D(dentry);
 	void *p;
-	int unlock_on_err = 0;
 
-	if (!info) {
-		dentry->d_fsdata = kmem_cache_alloc(unionfs_dentry_cachep,
-						    GFP_ATOMIC);
-		info = UNIONFS_D(dentry);
-		if (!info)
-			goto out;
+	BUG_ON(info);
 
-		mutex_init(&info->lock);
-		unionfs_lock_dentry(dentry);
-		unlock_on_err = 1;
+	dentry->d_fsdata = kmem_cache_alloc(unionfs_dentry_cachep,
+					    GFP_ATOMIC);
+	info = UNIONFS_D(dentry);
+	if (!info)
+	  goto out;
 
-		info->lower_paths = NULL;
-	}
-
-	mutex_lock(&info->lock);
+	mutex_init(&info->lock);
+	info->lower_paths = NULL;
+	unionfs_lock_dentry(dentry);
 
 	info->bstart = info->bend = info->bopaque = -1;
 	info->bcount = sbmax(dentry->d_sb);
@@ -518,10 +513,7 @@ int new_dentry_private_data(struct dentry *dentry)
 	return 0;
 
 out_free:
-	kfree(info->lower_paths);
-	if (unlock_on_err)
-		unionfs_unlock_dentry(dentry);
-
+	unionfs_unlock_dentry(dentry);
 out:
 	free_dentry_private_data(info);
 	dentry->d_fsdata = NULL;
