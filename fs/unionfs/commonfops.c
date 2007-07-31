be9cf470545642d87a2e28e506dc1a3910342477
Erez_Zadok <ezk@cs.sunysb.edu>
2007-07-31 03:58:50 -0400

    Unionfs: improved printk upon copyup
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- fs/unionfs/commonfops.c
+++ fs/unionfs/commonfops.c
@@ -392,8 +392,8 @@ int unionfs_file_revalidate(struct file *file, int willwrite)
 	if (willwrite && IS_WRITE_FLAG(file->f_flags) &&
 	    !IS_WRITE_FLAG(unionfs_lower_file(file)->f_flags) &&
 	    is_robranch(dentry)) {
-		printk(KERN_DEBUG "unionfs: doing delayed copyup of a "
-		       "read-write file on a read-only branch\n");
+		printk(KERN_DEBUG "unionfs: do delay copyup of \"%s\"\n",
+		       dentry->d_name.name);
 		err = do_delayed_copyup(file);
 	}
 
