e4b0503ab85573ee36c24247bdf1c0369d4df51f
Erez Zadok <ezk@cs.sunysb.edu>
2010-01-04 20:45:06 -0500

    VFS: export release_open_intent symbol
    Needed to release the resources of the lower nameidata structures that we
    create and pass to lower file systems (e.g., when calling vfs_create).
    
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- fs/namei.c
+++ fs/namei.c
@@ -574,6 +574,7 @@ void release_open_intent(struct nameidata *nd)
 			fput(file);
 	}
 }
+EXPORT_SYMBOL_GPL(release_open_intent);
 
 static inline int d_revalidate(struct dentry *dentry, struct nameidata *nd)
 {
